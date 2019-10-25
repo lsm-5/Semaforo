c5a7390dc69fcb0145f72cd29e268e1643cf75f1
lsm5 <lsm5@cin.ufpe.br>
2019-10-24 21:42:50 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,21 @@ routes.post('/semaforo/:id', (req,res) =>{
     const id = req.params.id;
     const { 
         tempoSinalVermelho, 
-        tempoSinalVerde, 
+        tempoSinalVerde,
+        tempoSinalAmarelo,
         quantidadeVeiculos, 
         velocidadeVia, 
         distanciaVia  
     } = req.body;
 
-    //fazer algo
 
-    return res.json({tempoSinalVermelho:"X",tempoSinlaVerde:"Y"});
+    var verm = 50, verde = 60, ama=5;
+
+    ama =  (velocidadeVia/5.6) - 10;
+    verm = ((distanciaVia + quantidadeVeiculos) / velocidadeVia)*0.13;
+    verde = (tempoSinalAmarelo/1) + (tempoSinalVermelho/1) + (tempoSinalVerde * 0.666)
+
+    return res.json({tempoSinalVermelho:verm,tempoSinalVerde:verde, tempoSinalAmarelo:ama});
 
 })
 
